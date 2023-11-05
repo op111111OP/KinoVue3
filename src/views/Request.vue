d3e8d549399a915ebaa2a34d95c18906337922cf
op111111OP <p47601758@gmail.com>
2023-11-05 16:37:06 +0200

    modified:   src/views/Request.vue

--- src/views/Request.vue
+++ src/views/Request.vue
@@ -55,7 +55,7 @@ export default {
          breakpoints: {
             // 700px and up
             500: {
-               itemsToShow: 2,
+               itemsToShow: 1,
                snapAlign: "center",
             },
             700: {
